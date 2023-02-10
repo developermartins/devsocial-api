dd29d0f54a7e92dc28876aa1c2338f2af8de553d
devLucasmarts <martssilvapro@gmail.com>
2023-02-10 18:27:06 -0300

    add envs

--- model/connect.js
+++ model/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-     host: "localhost",
-     user: "root",
-     password: "",
-     database: "social"
+     host: process.env.DB_HOST,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.database
 })
