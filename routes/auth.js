66f2ee195c897607bb077c486b603917e8c440dd
devLucasmarts <martssilvapro@gmail.com>
2023-02-10 18:23:38 -0300

    add basic auth controller & model structure & install dotenv

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {  } from "../controllers/authController.js";
+import { login, register, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("", "");
+router.post("login", login);
+router.post("register", register);
+router.post("logout", logout);
 
 export default router;
