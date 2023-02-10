e08063d6bce7cf3ac74de5027fc3ad66b3294452
devLucasmarts <martssilvapro@gmail.com>
2023-02-10 18:01:03 -0300

    config basic user route

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ import Express from "express";
 const app = Express();
 const port = 3001;
 
+import userRoutes from "./routes/users.js";
+
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}! 👌`));
