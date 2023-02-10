75b56bf5c77218da7eee2dfa3fc2946333a95ea1
devLucasmarts <martssilvapro@gmail.com>
2023-02-10 18:13:33 -0300

    import other routes

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ import Express from "express";
 const app = Express();
 const port = 3001;
 
+import authRoutes from "./routes/users.js";
 import userRoutes from "./routes/users.js";
+import commentsRoutes from "./routes/users.js";
+import likesRoutes from "./routes/users.js";
+import postsRoutes from "./routes/users.js";
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/comments", commentsRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/likes", likesRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}! 👌`));
