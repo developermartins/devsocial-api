fd589a59a9cdf4e6ff59d6d6fc5fe62d9745c647
devLucasmarts <martssilvapro@gmail.com>
2023-02-13 13:34:22 -0300

    config env

--- model/connect.js
+++ model/connect.js
@@ -1,11 +1,14 @@
+import * as dotenv from 'dotenv';
 import mysql from "mysql2/promise";
 
+dotenv.config();
+
 export const connection = mysql.createPool({
-     host: 'localhost',
-     port: 3306,
-     user: 'root',
-     password: 'devmarts@MartsDev2230',
-     database: 'social',
+     host: process.env.DB_HOST,
+     port: process.env.DB_PORT,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DATABASE,
      waitForConnections: true,
      connectionLimit: 10,
      queueLimit: 0,
