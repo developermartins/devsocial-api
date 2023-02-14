968ac0fd2bee5c677a136dd17471c734d90254ea
devLucasmarts <martssilvapro@gmail.com>
2023-02-14 10:28:40 -0300

    implements postsServices

---
+++ services/postsServices.js
@@ -0,0 +1,9 @@
+import { getPosts } from "../model/postsModel";
+
+export const getPostsServices = async () => {
+     const posts = await getPosts();
+
+     if (posts.length === 0) return { type: 'NOTHING_POSTS_FOUND', message: 'Nothing posts found.' };
+
+     return { type: null, message: posts };
+};
