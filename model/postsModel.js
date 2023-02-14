6e1bbaccda35edd890179d81a89d26b2790c2000
devLucasmarts <martssilvapro@gmail.com>
2023-02-14 11:21:40 -0300

    join relationship table

--- model/postsModel.js
+++ model/postsModel.js
@@ -2,7 +2,8 @@ import { connection } from "./connect.js";
 
 export const getPosts = async () => {
      const [result] = await connection.execute (
-          'SELECT p.*, u.id AS userId, name, profile_pic FROM posts AS p JOIN users AS u ON (u.id = p.userId)'
+          `SELECT p.*, u.id AS userId, name, profile_pic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+           JOIN relationship AS r ON (p.userId = r.followerUserId AND r.followerUserId = ?)`
      );
 
      return result;
