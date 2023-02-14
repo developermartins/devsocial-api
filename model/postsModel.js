05b95573f7d94b929f58d99321c6db8836b4e680
devLucasmarts <martssilvapro@gmail.com>
2023-02-14 10:22:47 -0300

    implements getPosts model

--- model/postsModel.js
+++ model/postsModel.js
@@ -1,2 +1,9 @@
 import { connection } from "./connect.js";
 
+export const getPosts = async () => {
+     const [result] = await connection.execute (
+          'SELECT * FROM posts'
+     );
+
+     return result;
+};
