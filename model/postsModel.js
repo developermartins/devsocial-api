6ff6ec4576815b0d99aa875cf7c5addbf854131a
devLucasmarts <martssilvapro@gmail.com>
2023-02-14 10:33:11 -0300

    add users in SQL query

--- model/postsModel.js
+++ model/postsModel.js
@@ -2,7 +2,7 @@ import { connection } from "./connect.js";
 
 export const getPosts = async () => {
      const [result] = await connection.execute (
-          'SELECT * FROM posts'
+          'SELECT * FROM posts AS p JOIN users AS u'
      );
 
      return result;
