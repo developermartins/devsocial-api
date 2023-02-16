e7eaf69a0afad68eada93ba2784c392eb245b737
devLucasmarts <martssilvapro@gmail.com>
2023-02-16 16:53:07 -0300

    implements like services

--- services/likeServices.js
+++ services/likeServices.js
@@ -1,3 +1,7 @@
-export const getLikeServices = async (postId) => {
+import { getLikes } from "../model/likeModel";
 
+export const getLikeServices = async (postId) => {
+     const res = await getLikes(postId);
+
+     return { type: null, message: res };
 };
