18284e5dfed7a338f8712d66fa352620366abd40
devLucasmarts <martssilvapro@gmail.com>
2023-02-16 11:14:01 -0300

    implements getCommentsServices

--- services/commentsServices.js
+++ services/commentsServices.js
@@ -1,3 +1,9 @@
-export const getCommentsServices = async () => {
+import { getComments } from "../model/commentsModel";
 
+export const getCommentsServices = async (postId) => {
+     const res = await getComments(postId);
+
+     if (res.length === 0) return { type: "NOT_FOUND", message: "No comments yet." };
+
+     return { type: null, message: res };
 };
