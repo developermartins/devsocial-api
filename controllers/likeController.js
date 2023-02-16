59c69f01bf2896b415215e0176f4393330c1171a
devLucasmarts <martssilvapro@gmail.com>
2023-02-16 17:21:16 -0300

    add like and dislike controllers

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -8,5 +8,43 @@ export const getLikes = async (req, res) => {
 
      const { message } = await getLikeServices(postId);
 
-     return res.status(200).json(message);
+     return res.status(200).json(message.map(like=>like.userId));
+};
+
+export const like = async (req, res) => {
+
+     const token = req.cookies.acessToken;
+
+     const { comment_content, postId } = req.body;
+
+     if (!token) return res.status(401).json("Not logged in!");
+
+     jwt.verify(token, process.env.SECRET, async function(err, userInfo) {
+          if (err) return res.status(403).json("Token is not valid!");
+
+          const { type, message }  = await addCommentServices(comment_content, postId, userInfo);
+     
+          if (type) return res.status(404).json(message);
+     
+          return res.status(200).json(message);
+     });
+};
+
+export const dislike = async (req, res) => {
+
+     const token = req.cookies.acessToken;
+
+     const { comment_content, postId } = req.body;
+
+     if (!token) return res.status(401).json("Not logged in!");
+
+     jwt.verify(token, process.env.SECRET, async function(err, userInfo) {
+          if (err) return res.status(403).json("Token is not valid!");
+
+          const { type, message }  = await addCommentServices(comment_content, postId, userInfo);
+     
+          if (type) return res.status(404).json(message);
+     
+          return res.status(200).json(message);
+     });
 };
--- routes/likes.js
+++ routes/likes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getLikes } from "../controllers/likeController.js";
+import { getLikes, like, dislike } from "../controllers/likeController.js";
 
 const router = express.Router();
 
 router.get("/", getLikes);
+router.post("/", like);
+router.delete("/", dislike);
 
 export default router;
