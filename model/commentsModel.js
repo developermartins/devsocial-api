782649503d6d599eaf5dde78b43cacad2fc93c9c
devLucasmarts <martssilvapro@gmail.com>
2023-02-16 11:11:10 -0300

    implements getComments model

--- model/commentsModel.js
+++ model/commentsModel.js
@@ -1,3 +1,12 @@
-export const getComments = async () => {
+import moment from "moment";
+import { connection } from "./connect.js";
 
+export const getComments = async (postId) => {
+     const [result] = await connection.execute (
+          `SELECT c.*, u.id AS userId, name, profile_pic FROM comments AS c JOIN users AS u ON (u.id = p.userId)
+          WHERE c.postId = ? ORDER BY c.create_time DESC`,
+          [postId]
+     );
+
+     return result;
 };
