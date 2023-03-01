257623288a3917a183a1001b3f0c9d2395ebf52d
devLucasmarts <martssilvapro@gmail.com>
2023-03-01 11:06:09 -0300

    add relationship model

---
+++ model/relationshipModel.js
@@ -0,0 +1,28 @@
+import { connection } from "./connect.js";
+
+export const getLikes = async (postId) => {
+     const [result] = await connection.execute (
+          `SELECT userId from likes WHERE postId = ?`,
+          [postId]
+     );
+
+     return result;
+};
+
+export const addLike = async (userId, postId) => {
+     const [result] = await connection.execute (
+         "INSERT INTO likes (`userId`, `postId`) VALUES (?, ?)",
+          [userId, postId]
+     );
+
+     return result;
+};
+
+export const addDislike = async (userId, postId) => {
+     const [result] = await connection.execute (
+         "DELETE FROM likes WHERE `userId` = ? AND `postId` = ?",
+          [userId, postId]
+     );
+
+     return result;
+};
