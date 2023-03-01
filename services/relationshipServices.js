864bad2c8d96d226e6d99fb76917cc1578de7702
devLucasmarts <martssilvapro@gmail.com>
2023-03-01 11:05:19 -0300

    change function name

--- services/relationshipServices.js
+++ services/relationshipServices.js
@@ -1,18 +1,18 @@
 import { addDislike, addLike, getLikes } from "../model/likeModel.js";
 
-export const getLikeServices = async (postId) => {
+export const getRelationshipServices = async (postId) => {
      const res = await getLikes(postId);
 
      return { type: null, message: res };
 };
 
-export const addLikeServices = async (userId, postId) => {
+export const addRelationshipServices = async (userId, postId) => {
      await addLike(userId, postId);
 
      return { type: null, message: "Post has been liked!" };
 };
 
-export const addDislikeServices = async (userId, postId) => {
+export const deleteRelationshipServices = async (userId, postId) => {
      await addDislike(userId, postId);
 
      return { type: null, message: "Like has been removed!" };
