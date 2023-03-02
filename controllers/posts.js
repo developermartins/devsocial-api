f0ba7c0225f9ad0d34e03905911d713c852ddca4
devLucasmarts <martssilvapro@gmail.com>
2023-03-02 15:08:26 -0300

    setup likePosts

--- controllers/posts.js
+++ controllers/posts.js
@@ -51,3 +51,30 @@ export const getUserPosts = async (req, res) => {
           res.status(404).json({ message: err.message });
      };
 };
+
+
+export const likePosts = async (req, res) => {
+     try {
+          const { id } = req.params;
+          const { userId } = req.body;
+
+          const post = await Post.findById(id);
+          const isLike = await post.likes.get(userId);
+
+          if (isLike) {
+               post.likes.delete(userId);
+          } else {
+               post.likes.set(userId, true);
+          };
+
+          const updatedPost = await Post.findByIdAndUpdate(
+               id,
+               { likes: post.likes },
+               { new: true }
+          );
+
+          res.status(200).json(updatedPost);
+     } catch (err) {
+          res.status(404).json({ message: err.message });
+     };
+};
