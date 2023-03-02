838e988049022621f534761502221469f36798bd
devLucasmarts <martssilvapro@gmail.com>
2023-03-02 08:51:42 -0300

    reset project

--- routes/comments.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { comments, addComment } from "../controllers/commentController.js";
-
-const router = express.Router();
-
-router.get("/", comments);
-router.post("/", addComment);
-export default router;
--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { posts, addPost } from "../controllers/postController.js";
-
-const router = express.Router();
-
-router.get("/", posts);
-router.post("/", addPost);
-
-export default router;
