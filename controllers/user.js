582392974ab3660058b4a7db2c942ccef465e89e
devLucasmarts <martssilvapro@gmail.com>
2023-03-02 12:14:21 -0300

    setup getUser

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+export const getUser = async (req, res) => {
+     try {
+          const { id } = req.params;
+
+          const user = User.findById(id);
+
+          res.status(200).json(user);
+     } catch (err) {
+          res.status(404).json({ message: err.message });
+     }
+}
