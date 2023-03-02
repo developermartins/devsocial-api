f1bde7f5b910d867fac20901ad699c11bef2024c
devLucasmarts <martssilvapro@gmail.com>
2023-03-02 14:31:53 -0300

    setup posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { getFeedPosts, getUserPosts, likePosts } from '../controllers/post.js';
+import { getFeedPosts, getUserPosts, likePosts } from '../controllers/posts.js';
 
 
 const router = express.Router();
