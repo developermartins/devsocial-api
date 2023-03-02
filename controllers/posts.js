8eca0e0249d9a4ccaa9116117232c412c810115b
devLucasmarts <martssilvapro@gmail.com>
2023-03-02 14:57:09 -0300

    setup getFeedPost controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,3 +27,16 @@ export const createPost = async (req, res) => {
           res.status(409).json({ message: err.message });
      };
 };
+
+
+export const getFeedPosts = async (req, res) => {
+     try {
+          const post = await Post.find();
+
+          res.status(200).json(post);
+     } catch (err) {
+          res.status(404).json({ message: err.message });
+     };
+};
+
+
