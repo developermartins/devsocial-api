db13a006e04da05f97b7aeee7577338af838ebb7
devLucasmarts <martssilvapro@gmail.com>
2023-03-08 20:39:08 -0300

    add _

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
 };
 
 
-export const getFeedPosts = async (req, res) => {
+export const getFeedPosts = async (_req, res) => {
      try {
           const post = await Post.find();
 
